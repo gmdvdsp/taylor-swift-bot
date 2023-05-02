4da9f8c80578b6398c21993cee90544641ad699d
gmdvdsp <90121883+gmdvdsp@users.noreply.github.com>
2023-05-01 18:37:33 -0700

    Remove bot key

--- bot.py
+++ bot.py
@@ -61,4 +61,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run('NTY5MjE1NDg2MDc2NjQ5NDg1.GkhadZ.Jn5r-8Qr3E-_OIC2wAIYqCF5ts3FnZDhQgn9fs')
+bot.run(#BOT KEY#)
