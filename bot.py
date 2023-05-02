5a05b8391add4404b82c0bd6badc06614119191e
gmdvdsp <90121883+gmdvdsp@users.noreply.github.com>
2023-05-01 18:40:03 -0700

    Remove bot key

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import datetime as time
 import database
 import discord
 from discord.ext import commands
-# token: NTY5MjE1NDg2MDc2NjQ5NDg1.GkhadZ.Jn5r-8Qr3E-_OIC2wAIYqCF5ts3FnZDhQgn9fs
 
 AUTHOR_EMBED_URL = "https://i.imgur.com/6DSv0Su.jpg"
 RED = 0xFF0000
@@ -59,4 +58,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run('NTY5MjE1NDg2MDc2NjQ5NDg1.GkhadZ.Jn5r-8Qr3E-_OIC2wAIYqCF5ts3FnZDhQgn9fs')
+bot.run(#BOTKEY)
